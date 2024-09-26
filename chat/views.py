4aa07fe7e22ed60aa19ed4c31218782361837993
chaiwat007x <161495586+chaiwat007x@users.noreply.github.com>
2024-09-26 12:04:05 +0700

    add chat design

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+def chat_view(request):
+    return TemplateView.as_view(template_name='chat_page/chat.html')(request)
